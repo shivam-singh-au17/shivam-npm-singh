b55b6b8265afee6a1434df38d1273dc52c473603
Shivam Singh <shivamsingh4458@gmail.com>
2022-11-14 20:51:41 +0530

    Last commit for deleting from local

---
+++ test.js
@@ -0,0 +1,9 @@
+const { shivamPrintFunc } = require("shivam-privet-npm");
+
+const myPrint = new shivamPrintFunc("Shivam", 23, "Mirzapur");
+
+myPrint.printData();
+
+myPrint.printName();
+
+myPrint.printAge();
